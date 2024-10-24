cc9fee28a15f1b32d60f055eab2eb8f36d420eb0
solankichirag2903 <solankichirag7279@gmail.com>
2024-10-25 00:06:03 +0530

    everything updated

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open('README.md', 'r', encoding='utf-8') as f:
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "CMSOLANKI"
+AUTHOR_EMAIL = "chiragsolanki2629@gmail.com"
 
 setup(
     name=PKG_NAME,
